da0220d865895ca2b4c1579137a17473a962f5f3
MSV-School <MSV-School@MSV-SCHOOL>
2021-07-22 15:31:08 +0200

    Section 5 - Expanding Navigation.

--- js/menuToggle.js
+++ js/menuToggle.js
@@ -1,8 +1,23 @@
-//Create a button and give it an ID of "hamburgerBtn"
-//Attach an ID of "primaryNav" to the UL of the unordered list that holds your links
+/*
+ * Create a button and give it an ID of "hamburgerBtn"
+ * Attach an ID of "primaryNav" to the UL of the unordered list that holds your links
+ */
 function toggleMenu() {
+    var nav = document.getElementById("primaryNav");
+
+    if (nav.className === "closed") {
+        nav.className = "open";
+    }
+    else {
+        nav.className = "closed";
+    }
+}
+var btn = document.getElementById('hamburgerBtn');
+btn.onclick = toggleMenu;
+
+//********** INDEX.HTML V1 **********
+/*function toggleMenu() {
     document.getElementById("primaryNav").classList.toggle("closed");
 }
-
 var btn = document.getElementById('hamburgerBtn');
-btn.onclick = toggleMenu; 
+btn.onclick = toggleMenu;*/
