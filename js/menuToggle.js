e7cfa70191005ae9a6491ba7eef3d9e419f93b6d
MSV-School <MSV-School@MSV-SCHOOL>
2021-07-22 20:00:31 +0200

    Section 8 - Single Level Navigation.

--- js/menuToggle.js
+++
@@ -1,8 +0,0 @@
-//Create a button and give it an ID of "hamburgerBtn"
-//Attach an ID of "primaryNav" to the UL of the unordered list that holds your links
-function toggleMenu() {
-    document.getElementById("hamburgerBtn").classList.toggle("open");
-    document.getElementById("primaryNav").classList.toggle("open");
-}
-var btn = document.getElementById('hamburgerBtn');
-btn.onclick = toggleMenu; 
